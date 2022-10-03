3e1eca677c3aa28c9a2fe7a04a3b42d0f71db442
Santiago1916 <62898718+Santiago1916@users.noreply.github.com>
2022-10-03 17:16:23 -0500

    models

--- HM_Cocktails/cocktails/models.py
+++ HM_Cocktails/cocktails/models.py
@@ -2,37 +2,23 @@ from django.db import models
 from .choices import SinoCon
 
 # Create your models here.
-class usuario(models.Model):
+class Usuario(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID')                
     nombre = models.CharField(max_length = 50, verbose_name = 'Nombre usuario')
     apellido = models.CharField(max_length = 50, verbose_name = 'Apellido usuario')
     nombreDeUsuario = models.CharField(max_length = 50, verbose_name = 'Nombre de usuario')
     fechaNacimiento = models.DateField(verbose_name= 'Fecha nacimiento usuario')
-    '''idListaFavoritos = models.ForeignKey(favoritos, null = True, blank = True)'''
 
     def usuarioDatos(self):
         return "{}, {} {} {}".format(self.id, self.nombreDeUsuario, self.nombre, self.apellido)
 
     def __str__(self):
         return self.usuarioDatos()
-
-class componente(models.Model):
-    id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID') 
-    nombre = models.CharField(max_length = 50, verbose_name = 'Nombre componente')
-    tipoComponente = models.CharField(max_length = 50, verbose_name = 'Tipo componente')
-
-    def componenteDatos(self):
-        return "{}, {} {}".format(self.id, self.nombre, self.tipoComponente)
-
-    def __str__(self):
-        return self.componenteDatos()
-
-class coctel(models.Model):
+        
+class Coctel(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID') 
     nombre = models.TextField(blank = True)
-    idComponente = models.ForeignKey(componente, null = True, blank = True, on_delete = models.CASCADE)
     sinoAlcohol =  models.CharField(max_length = 2, choices = SinoCon, default = 'Si')
-    '''favorito = models.ForeignKey(models.favoritos, null = True, blank = True)'''
 
     def coctelDatos(self):
         return "{} {}, {}".format(self.id,  self.nombre , self.sinoAlcohol)
@@ -40,17 +26,29 @@ class coctel(models.Model):
     def __str__(self):
         return self.coctelDatos()
 
+class Ingrediente(models.Model):
+    id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID')
+    idCoctel = models.ForeignKey(Coctel, null = True, blank = True, on_delete = models.CASCADE)
+    nombre = models.CharField(max_length = 50, verbose_name = 'Nombre del coctel')
+    cantidad = models.CharField(max_length = 50, verbose_name = 'Cantidad del alcohol')
+    tipoIngrediente = models.CharField(max_length = 50, verbose_name = 'Tipo componente')
+
+    def componenteDatos(self):
+        return "{}, {} {}".format(self.id, self.nombre, self.tipoComponente)
+
+    def __str__(self):
+        return self.componenteDatos()
+
 class favoritos(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID') 
-    idUsuario = models.ForeignKey(usuario,null = True, blank = True, on_delete = models.CASCADE)
-    idCoctel = models.ForeignKey(coctel,null = True, blank = True, on_delete = models.CASCADE)
+    idUsuario = models.ForeignKey(Usuario, null = True, blank = True, on_delete = models.CASCADE)
+    idCoctel = models.ForeignKey(Coctel, null = True, blank = True, on_delete = models.CASCADE)
 
     def favoritosDatos(self):
         return "{}, {}, {}".format(self.id,  self.idUsuario , self.idCoctel)
     
     def __str__(self):
         return self.favoritosDatos()
-
 
 """ Relaciones Foraneas entre modelos -> https://www.youtube.com/watch?v=y89VHGofsDQ"""
 '''Django & PostgreSQL CRUD -> https://www.youtube.com/watch?v=_zNZ1lK6RTA'''
