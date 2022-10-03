4542faecb85d3a32a8368801beacfe84141dd9fd
SamMartSan <samymartsa123@gmail.com>
2022-10-03 15:10:21 -0500

    Update models.py

--- HM_Cocktails/cocktails/models.py
+++ HM_Cocktails/cocktails/models.py
@@ -2,54 +2,54 @@ from django.db import models
 from .choices import SinoCon
 
 # Create your models here.
-class usuario(models.Model):
+class Usuario(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID')                
-    nombre = models.CharField(max_length = 50, verbose_name = 'Nombre usuario')
-    apellido = models.CharField(max_length = 50, verbose_name = 'Apellido usuario')
-    nombreDeUsuario = models.CharField(max_length = 50, verbose_name = 'Nombre de usuario')
-    fechaNacimiento = models.DateField(verbose_name= 'Fecha nacimiento usuario')
-    '''idListaFavoritos = models.ForeignKey(favoritos, null = True, blank = True)'''
+    nombre = models.CharField(max_length = 50, verbose_name = 'Nombre Usuario')
+    apellido = models.CharField(max_length = 50, verbose_name = 'Apellido Usuario')
+    nombreDeUsuario = models.CharField(max_length = 50, verbose_name = 'Nombre de Usuario')
+    fechaNacimiento = models.DateField(verbose_name= 'Fecha nacimiento Usuario')
+    '''idListaFavoritos = models.ForeignKey(Favoritos, null = True, blank = True)'''
 
-    def usuarioDatos(self):
+    def UsuarioDatos(self):
         return "{}, {} {} {}".format(self.id, self.nombreDeUsuario, self.nombre, self.apellido)
 
     def __str__(self):
-        return self.usuarioDatos()
+        return self.UsuarioDatos()
 
-class componente(models.Model):
+class Componente(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID') 
-    nombre = models.CharField(max_length = 50, verbose_name = 'Nombre componente')
-    tipoComponente = models.CharField(max_length = 50, verbose_name = 'Tipo componente')
+    nombre = models.CharField(max_length = 50, verbose_name = 'Nombre Componente')
+    tipoComponente = models.CharField(max_length = 50, verbose_name = 'Tipo Componente')
 
-    def componenteDatos(self):
+    def ComponenteDatos(self):
         return "{}, {} {}".format(self.id, self.nombre, self.tipoComponente)
 
     def __str__(self):
-        return self.componenteDatos()
+        return self.ComponenteDatos()
 
-class coctel(models.Model):
+class Coctel(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID') 
     nombre = models.TextField(blank = True)
-    idComponente = models.ForeignKey(componente, null = True, blank = True, on_delete = models.CASCADE)
+    idComponente = models.ForeignKey(Componente, null = True, blank = True, on_delete = models.CASCADE)
     sinoAlcohol =  models.CharField(max_length = 2, choices = SinoCon, default = 'Si')
-    '''favorito = models.ForeignKey(models.favoritos, null = True, blank = True)'''
+    '''favorito = models.ForeignKey(models.Favoritos, null = True, blank = True)'''
 
-    def coctelDatos(self):
+    def CoctelDatos(self):
         return "{} {}, {}".format(self.id,  self.nombre , self.sinoAlcohol)
     
     def __str__(self):
-        return self.coctelDatos()
+        return self.CoctelDatos()
 
-class favoritos(models.Model):
+class Favoritos(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID') 
-    idUsuario = models.ForeignKey(usuario,null = True, blank = True, on_delete = models.CASCADE)
-    idCoctel = models.ForeignKey(coctel,null = True, blank = True, on_delete = models.CASCADE)
+    idUsuario = models.ForeignKey(Usuario,null = True, blank = True, on_delete = models.CASCADE)
+    idCoctel = models.ForeignKey(Coctel,null = True, blank = True, on_delete = models.CASCADE)
 
-    def favoritosDatos(self):
+    def FavoritosDatos(self):
         return "{}, {}, {}".format(self.id,  self.idUsuario , self.idCoctel)
     
     def __str__(self):
-        return self.favoritosDatos()
+        return self.FavoritosDatos()
 
 
 """ Relaciones Foraneas entre modelos -> https://www.youtube.com/watch?v=y89VHGofsDQ"""
