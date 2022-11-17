8fdee314ef1c9013f5b6a5cd15e89c2f794650dc
SamMartSan <samymartsa123@gmail.com>
2022-11-17 12:33:41 -0500

    first template

--- HM_Cocktails/cocktails/views.py
+++ HM_Cocktails/cocktails/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template , Context
 
+def saludo(request):
+    doc_externo=open("C:\Users\usuario\Dropbox\My PC (DESKTOP-23VV5U8)\Documents\GitHub\LabSoft3\HM_Cocktails\HM_Cocktails\templates\inicio.html")
+    ptl = Template(doc_externo.read)
+    doc_externo.close
+    ctx = Context()
+    documento = ptl.render(ctx)
+    return HttpResponse(documento)
 # Create your views here.
