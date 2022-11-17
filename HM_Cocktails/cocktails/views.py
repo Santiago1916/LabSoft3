6559dd393c6d1683eea4045e0f1d2845376e28cf
SamMartSan <samymartsa123@gmail.com>
2022-11-17 14:49:29 -0500

    .

--- HM_Cocktails/cocktails/views.py
+++ HM_Cocktails/cocktails/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Template , Context
+from django.template import Template , Context , loader
 
 def saludo(request):
-    doc_externo=open("C:/Users/usuario/Dropbox/My PC (DESKTOP-23VV5U8)/Documents/GitHub/LabSoft3/HM_Cocktails/HM_Cocktails/templates/inicio.html")
-    ptl = Template(doc_externo.read)
-    doc_externo.close
-    ctx = Context()
-    documento = ptl.render(ctx)
-    return HttpResponse(documento)
+    template=loader.get_template("inicio.html")
+    return HttpResponse(template.render())
+    
 
 def login(request):
     doc_externo=open("C:/Users/usuario/Dropbox/My PC (DESKTOP-23VV5U8)/Documents/GitHub/LabSoft3/HM_Cocktails/HM_Cocktails/templates/login.html")
