fce4c339c6a32d28a37b3651b7eb7c5537dfd7e4
SamMartSan <samymartsa123@gmail.com>
2022-11-17 12:59:37 -0500

    login template

--- HM_Cocktails/cocktails/views.py
+++ HM_Cocktails/cocktails/views.py
@@ -3,7 +3,15 @@ from django.http import HttpResponse
 from django.template import Template , Context
 
 def saludo(request):
-    doc_externo=open("C:\Users\usuario\Dropbox\My PC (DESKTOP-23VV5U8)\Documents\GitHub\LabSoft3\HM_Cocktails\HM_Cocktails\templates\inicio.html")
+    doc_externo=open("C:/Users/usuario/Dropbox/My PC (DESKTOP-23VV5U8)/Documents/GitHub/LabSoft3/HM_Cocktails/HM_Cocktails/templates/inicio.html")
+    ptl = Template(doc_externo.read)
+    doc_externo.close
+    ctx = Context()
+    documento = ptl.render(ctx)
+    return HttpResponse(documento)
+
+def login(request):
+    doc_externo=open("C:/Users/usuario/Dropbox/My PC (DESKTOP-23VV5U8)/Documents/GitHub/LabSoft3/HM_Cocktails/HM_Cocktails/templates/login.html")
     ptl = Template(doc_externo.read)
     doc_externo.close
     ctx = Context()
