de3f43d401cf2f288267806df93eaed05742bc56
Santiago1916 <62898718+Santiago1916@users.noreply.github.com>
2022-11-20 22:57:48 -0500

    models

--- HM_Cocktails/cocktails/admin.py
+++ HM_Cocktails/cocktails/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Usuario, CategoriaCoctel, Coctel, Imagen, Licor, Diluidores, ComponentesCoctel, Favoritos
+
+admin.site.register(Usuario)
+admin.site.register(CategoriaCoctel)
+admin.site.register(Coctel)
+admin.site.register(Imagen)
+admin.site.register(Licor)
+admin.site.register(Diluidores)
+admin.site.register(ComponentesCoctel)
+admin.site.register(Favoritos)
--- HM_Cocktails/cocktails/models.py
+++ HM_Cocktails/cocktails/models.py
@@ -1,34 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Usuario(models.Model):
-    id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID')                
-    nombre = models.CharField(max_length = 50, verbose_name = 'Nombre usuario')
-    apellido = models.CharField(max_length = 50, verbose_name = 'Apellido usuario')
-    nombreDeUsuario = models.CharField(max_length = 50, verbose_name = 'Nombre de usuario')
-    fechaNacimiento = models.DateField(verbose_name= 'Fecha nacimiento usuario')
+class Usuario(AbstractUser):
+    idUsuario = models.CharField(max_length=100, blank=True, null=True)
 
-    def usuarioDatos(self):
-        return "{}, {} {} {}".format(self.id, self.nombreDeUsuario, self.nombre, self.apellido)
+    def say_hello(self):
+        return "Hola, mi usuario es {}".format(self.first_name)
 
     def __str__(self):
-        return self.usuarioDatos()
-        
+        return f'{self.username}'
+
+class CategoriaCoctel(models.Model):
+    nombreCategoria = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Categoria Coctel'
+        verbose_name_plural = 'Categoria de cocteles'
+    
+    def __str__(self):
+        return f'{self.nombreCategoria}'
+
+    @property
+    def contarCoctelesPorCategoria(self):
+        return Coctel.objects.filter(category=self).count()
+
 class Coctel(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID') 
     nombre = models.TextField(max_length = 50, verbose_name = 'Nombre Coctel')
     sinoAlcohol =  models.BooleanField(default = 'True')
+    categoria = models.ForeignKey(CategoriaCoctel, on_delete=models.CASCADE, related_name="Cocteles categoria")
 
     def coctelDatos(self):
         return "{} {}, {}".format(self.id,  self.nombre , self.sinoAlcohol)
     
     def __str__(self):
-        return self.coctelDatos()
+        return f'{self.nombre}- categoria: {self.categoria}'
+
+class Imagen(models.Model):
+    coctel = models.ForeignKey(Coctel, on_delete=models.CASCADE, related_name='Obtener Imagen')
+    imagen = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return f'{self.imagen}'
 
 class Licor(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID')
     nombre = models.TextField(max_length = 50, verbose_name = 'Nombre del licor')
-    cantidad = models.IntegerField(verbose_name = 'Cantidad en ml del licor')
+    cantidad = models.IntegerField(verbose_name = 'Cantidad en ml del licor', default=0)
 
     def licorDatos(self):
         return "{}, {} {}".format(self.id,  self.nombre , self.cantidad)
@@ -50,8 +69,8 @@ class Diluidores(models.Model):
 class ComponentesCoctel(models.Model):
     id = models.BigAutoField(auto_created = True, primary_key = True, serialize = False, verbose_name = 'ID')
     idCoctel = models.ForeignKey(Coctel, null = True, blank = True, on_delete = models.CASCADE)
-    idLicorBase = models.ForeignKey(Licor, null = True, blank = True, on_delete = models.CASCADE)
-    idDiluidores = models.ForeignKey(Diluidores, null = True, blank = True, on_delete = models.CASCADE)
+    idLicorBase = models.ManyToManyField(Licor, null = True, blank = True, on_delete = models.CASCADE)
+    idDiluidores = models.ManyToManyField(Diluidores, null = True, blank = True, on_delete = models.CASCADE)
 
     def componenteDatos(self):
         return "{} {}, {} {} {}".format(self.id, self.idCoctel, self.idLicorBase, self.idLicorSecundario, self.idDiluidores)
